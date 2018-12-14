390e71a15dae1d7a92b4d214f65ea4da9f7f5abf
Angela Wolff <warriorwomenblog@gmail.com>
2018-12-14 12:37:12 +0000

    Adding routes to enable story options to be POSTed. Requires refactor

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ class MyRackApp < Sinatra::Base
 
   before do
     @story = Story.instance
+    puts '[Params]'
+    p params
   end
   
   get '/' do
@@ -18,8 +20,59 @@ class MyRackApp < Sinatra::Base
   get '/pages/:pageid' do
     # @story = session[:story]
     @pageid = params[:pageid]
+    p @pageid
     @page = @story.pages[@pageid.to_i]
     erb :page
   end
+
+  post '/pages/:pageid/option1' do
+    @pageid = params[:pageid]
+    p @pageid
+    @page = @story.pages[@pageid.to_i]
+    @option1 = Option.new(params[:option1])
+    @page.option1 = @option1
+    redirect '/pages/@pageid'
+  end
+
+  post '/pages/:pageid/option2' do
+    @pageid = params[:pageid]
+    p @pageid
+    @page = @story.pages[@pageid.to_i]
+    @option2 = Option.new(params[:option2])
+    @page.option2 = @option2
+    redirect '/pages/@pageid'
+  end
+
+  post '/pages/:pageid/option3' do
+    @pageid = params[:pageid]
+    p @pageid
+    @page = @story.pages[@pageid.to_i]
+    @option3 = Option.new(params[:option3])
+    @page.option3 = @option3
+    redirect '/pages/@pageid'
+  end
+
+  post '/pages/:pageid/option4' do
+    @pageid = params[:pageid]
+    p @pageid
+    @page = @story.pages[@pageid.to_i]
+    @option4 = Option.new(params[:option4])
+    @page.option4 = @option4
+    redirect '/pages/@pageid'
+  end
+
+  # post '/bookmarks' do
+  #   if Bookmark.create(params)
+  #     flash[:success] = "Successfully added bookmark"
+  #   else
+  #     flash[:error] = "Please enter a valid URL"
+  #   end
+  #   redirect '/bookmarks'
+  # end
+
+  # post '/bookmarks/:id/delete' do
+  #   Bookmark.delete(params['id'])
+  #   redirect '/bookmarks'
+  # end
 
 end
