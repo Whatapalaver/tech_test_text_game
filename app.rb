075600242a871ffdd55e3081ef922a30e951e4b9
Angela Wolff <warriorwomenblog@gmail.com>
2018-12-16 10:44:42 +0000

    Reducing POST route handlers to one with dynamic form number

--- app.rb
+++ app.rb
@@ -25,54 +25,17 @@ class MyRackApp < Sinatra::Base
     erb :page
   end
 
-  post '/pages/:pageid/option_1' do
+  post '/pages/:pageid/:formNum' do
     @pageid = params[:pageid]
+    @option_number = params[:formNum].to_i
+    @option_index = @option_number - 1
     @current_page = @story.pages[@pageid.to_i]
     ## create new option based on input submitted
-    @option_1 = Option.new(params[:option_1])
+    @option = Option.new(params[:option])
     ## store option in current page object
-    @current_page.options[0] = @option_1
+    @current_page.options[@option_index] = @option
     ## create a new page object based on option created and update story object
-    @new_page = @option_1.page_link
-    @story.pages.push(@new_page)
-    redirect "/pages/#{@pageid}"
-  end
-
-  post '/pages/:pageid/option_2' do
-    @pageid = params[:pageid]
-    @current_page = @story.pages[@pageid.to_i]
-    ## create new option based on input submitted
-    @option_2 = Option.new(params[:option_2])
-    ## store option in current page object
-    @current_page.options[1] = @option_2
-    ## create a new page object based on option created and update story object
-    @new_page = @option_2.page_link
-    @story.pages.push(@new_page)
-    redirect "/pages/#{@pageid}"
-  end
-
-  post '/pages/:pageid/option_3' do
-    @pageid = params[:pageid]
-    @current_page = @story.pages[@pageid.to_i]
-    ## create new option based on input submitted
-    @option_3 = Option.new(params[:option_3])
-    ## store option in current page object
-    @current_page.options[2] = @option_3
-    ## create a new page object based on option created and update story object
-    @new_page = @option_3.page_link
-    @story.pages.push(@new_page)
-    redirect "/pages/#{@pageid}"
-  end
-
-  post '/pages/:pageid/option_4' do
-    @pageid = params[:pageid]
-    @current_page = @story.pages[@pageid.to_i]
-    ## create new option based on input submitted
-    @option_4 = Option.new(params[:option_4])
-    ## store option in current page object
-    @current_page.options[3] = @option_4
-    ## create a new page object based on option created and update story object
-    @new_page = @option_4.page_link
+    @new_page = @option.page_link
     @story.pages.push(@new_page)
     redirect "/pages/#{@pageid}"
   end
